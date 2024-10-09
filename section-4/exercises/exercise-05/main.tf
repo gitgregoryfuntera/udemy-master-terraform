7e20e29ebc10abbf58b478c5488e6aa73b1a2034
gitgregoryfuntera <gregory.funtera@gmail.com>
2024-10-09 21:37:57 +0800

    - terraform exercises

--- section-4/exercises/exercise-05/main.tf
+++ section-4/exercises/exercise-05/main.tf
@@ -1,24 +1,12 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "ap-southeast-1"
-}
-
-resource "random_id" "bucket_suffix" {
-  byte_length = 6
-}
-
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "example-bucket-${random_id.bucket_suffix.hex}"
-}
-
-output "bucket_name" {
-  value = aws_s3_bucket.example_bucket.bucket
+  region = "ap-southeast-1"
 }
