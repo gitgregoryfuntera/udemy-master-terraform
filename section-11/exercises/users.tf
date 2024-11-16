4c5c6ecf8dc24d503c7994b70c977a094a384cb3
gitgregoryfuntera <gregory.funtera@gmail.com>
2024-11-16 22:15:20 +0800

    - terraform exercises section 11

--- section-11/exercises/users.tf
+++ section-11/exercises/users.tf
@@ -1,5 +1,8 @@
 locals {
   users_from_yaml = yamldecode(file("${path.module}/user-roles.yaml")).users
+  users_map = {
+    for user_config in local.users_from_yaml : user_config.username => user_config.roles
+  }
 }
 
 resource "aws_iam_user" "users" {
@@ -11,6 +14,25 @@ resource "aws_iam_user" "users" {
   }
 }
 
-output "users" {
-  value = local.users_from_yaml
+resource "aws_iam_user_login_profile" "login_profile" {
+  for_each        = aws_iam_user.users
+  user            = each.value.name
+  password_length = 8
+
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key
+    ]
+  }
 }
+
+output "user_credentials" {
+  sensitive = true
+  value     = nonsensitive({ for user, login_profile in aws_iam_user_login_profile.login_profile : user => login_profile.password })
+}
+
+# output "users_map" {
+#   value = local.flatten_users_map
+# }
